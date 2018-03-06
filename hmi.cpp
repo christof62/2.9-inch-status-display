5160825a7e82863e4b3ed9bc5fa16247b53bd890
christof62 <christof@menzenbachs.de>
2018-03-06 22:03:42 +0100

    Ino file renamed

--- hmi.cpp
+++ hmi.cpp
@@ -31,12 +31,6 @@ GxEPD_Class display(io); // default selection of D4, D2
 
 #elif defined(ESP32)
 
-// pins_arduino.h, e.g. LOLIN32
-// static const uint8_t SS    = 5;
-// static const uint8_t MOSI  = 23;
-// static const uint8_t MISO  = 19;
-// static const uint8_t SCK   = 18;
-
 // GxIO_SPI(SPIClass& spi, int8_t cs, int8_t dc, int8_t rst = -1, int8_t bl = -1);
 GxIO_Class io(SPI, SS, 17, 16); // arbitrary selection of 17,16
 // GxGDEP015OC1(GxIO& io, uint8_t rst = D4, uint8_t busy = D2);
